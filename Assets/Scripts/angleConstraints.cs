7d789d9cb5d5111ad147d237d665a2b2bf86cabc
Unknown <bernatriupuyal@enti.cat>
2018-10-25 10:57:00 +0200

    classe 25/10/2018

---
+++ Assets/Scripts/angleConstraints.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angleConstraints : MonoBehaviour
+{
+    public bool active;
+
+    [Range(0.0f, 180.0f)]
+    public float maxAngle;
+
+    [Range(0.0f, 180.0f)]
+    public float minAngle;
+
+    public Transform parent;
+    public Transform child;
+
+
+
+    void Start()
+    {
+    }
+
+    void LateUpdate()
+    {
+        
+
+        if (active)
+        {
+            //solve your exercise here
+            //Debug.Log(Vector3.Angle(parent.up, child.up));
+            if(Vector3.Angle(parent.up,child.up) > maxAngle)
+            {
+                Vector3 axis;
+                float angle;
+
+                Quaternion diference = Quaternion.Inverse(parent.rotation) * child.rotation;
+
+                diference.ToAngleAxis(out angle, out axis);
+
+                child.rotation = parent.rotation;
+
+                child.Rotate(axis ,maxAngle);
+
+
+            }
+
+
+
+        }
+    }
+
+    //add auxiliary functions, if needed, below
+
+
+
+
+}
---
+++ Assets/Scripts/trunme.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trunme : MonoBehaviour {
+
+
+
+
+    [SerializeField] GameObject joint1;
+    [SerializeField] GameObject joint2;
+    //[SerializeField]
+
+    private Vector3 axis1;
+    private Vector3 axis2;
+    private float angle1;
+    private float angle2;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+
+	}
+}
