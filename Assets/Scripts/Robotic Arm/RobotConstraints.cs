c396ef162991fe47b21d35977d1ce9350a761903
Bernat Riu <bernatriupuyal@enti.cat>
2018-11-15 10:52:45 +0100

    classe 15/11/2018: crec que m ho he carregat tot una miqueta

--- Assets/Scripts/Robotic Arm/RobotConstraints.cs
+++ Assets/Scripts/Robotic Arm/RobotConstraints.cs
@@ -2,28 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RobotConstraints : MonoBehaviour {
+public class RobotConstraints : MonoBehaviour
+{
 
 
-    public Transform[] Joints;
+    //public Transform[] Joints;
 
-    public float[] maxAngles;
+    public Vector2[] Min_MaxAngles;
 
     RoboticArmControl controlScript;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         controlScript = GetComponent<RoboticArmControl>();
 
-	}
-	
-	// Update is called once per frame
-	void lateUpdate () {
-		
-
-        //if(Vector3.Angle)
 
 
-	}
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        for (int i = 0; i < controlScript.Joints.Length; i++)
+        {
+            controlScript.angles[i] = Mathf.Clamp(controlScript.angles[i], Min_MaxAngles[i].x, Min_MaxAngles[i].y);
+
+        }
+
+
+    }
 }
--- Assets/Scripts/Robotic Arm/RoboticArmControl.cs
+++ Assets/Scripts/Robotic Arm/RoboticArmControl.cs
@@ -2,61 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoboticArmControl : MonoBehaviour {
+public class RoboticArmControl : MonoBehaviour
+{
 
 
 
-    public GameObject Joint0;
-    public GameObject Joint1;
-    public GameObject Joint2;
-    public GameObject Joint3;
-    public GameObject Joint4;
+    public GameObject[] Joints;
 
 
-    public float angle0;
-    public float angle1;
-    public float angle2;
-    public float angle3;
-    public float angle4;
 
-    private Quaternion Rot0;
-    private Quaternion Rot1;
-    private Quaternion Rot2;
-    private Quaternion Rot3;
-    private Quaternion Rot4;
+    public float[] angles;
 
 
-    public Vector3[] axis;
+    private Quaternion[] initRots;
+
+
+
+    private Vector3[] axis;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+
+        initRots = new Quaternion[5];
 
         axis = new Vector3[5] { Vector3.up, Vector3.right, Vector3.right, Vector3.right, Vector3.up };
 
-        //lerpC = 0;
-                                                                      
-        Rot0 = Joint0.transform.localRotation;                             
-        Rot1 = Joint1.transform.localRotation;                             
-        Rot2 = Joint2.transform.localRotation;   
-        Rot3 = Joint3.transform.localRotation;
-        Rot4 = Joint4.transform.localRotation;
+        for (int i = 0; i < Joints.Length; i++)
+        {
+            initRots[i] = Joints[i].transform.localRotation;
+
+        }
+
+
+
 
     }
 
     // Update is called once per frame
-    void Update () {
 
-        //lerpC += Time.deltaTime*0.3f;
+    void Update()
+    {
 
+        for (int i = 0; i < Joints.Length; i++)
+        {
+            Joints[i].transform.localRotation = initRots[i] * Quaternion.AngleAxis(angles[i], axis[i]);
 
+        }
 
-        Joint0.transform.localRotation = Rot0 * Quaternion.AngleAxis(angle0, Vector3.up);
-        Joint1.transform.localRotation = Rot1 * Quaternion.AngleAxis(angle1, Vector3.right);
-        Joint2.transform.localRotation = Rot2 * Quaternion.AngleAxis(angle2, Vector3.right);
-        Joint3.transform.localRotation = Rot3 * Quaternion.AngleAxis(angle3, Vector3.right);
-        Joint4.transform.localRotation = Rot4 * Quaternion.AngleAxis(angle4, Vector3.up);
-     
 
 
 
